a1c7829122df8778b53b3b5792fd6d0caf916fc7
Samyr Marjoub <samirgamesmarjoub@gmail.com>
2024-07-16 22:59:50 -0300

    new Component, noChannel Main Function

---
+++ app/components/main/main/nochannelselected.tsx
@@ -0,0 +1,22 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+import noChatSelected from '@/public/noChatSelected.svg'
+import Image from 'next/image'
+
+export default function nochannelselected() {
+  return (
+    <Box display={'flex'} justifyContent={'center'} alignItems={'center'} width={'100%'} height={'100%'} bg={'#202225'}>
+
+      <Box display={'flex'} justifyContent={'center'} flexDir={'column'} alignItems={'center'}>
+        <Image  src={noChatSelected} alt={''} />
+
+        <Box width={'400px'} >
+          <Text mt={'10px'} mb={'10px'} textAlign={'center'} fontWeight={'800'} fontSize={'18px'} color={'#B9BBBE'}>SEM CANAIS DE TEXTO
+          </Text>
+          <Text textAlign={'center'} color={'#A3A6AA'}>Você se vê em um lugar estranho. Você não tem acesso a nenhum canal de texto, ou não há nenhum neste servidor </Text>
+        </Box>
+      </Box>
+
+    </Box>
+  )
+}
--- app/components/main/mainapp.tsx
+++ app/components/main/mainapp.tsx
@@ -1,11 +1,11 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
-
+import Nochannelselected from './main/nochannelselected'
 
 export default function mainapp() {
   return (
-    <Box w={'100%'} bg={'#313338'}>
-
+    <Box w={'100%'} bg={'#313338'} height={'100%'}>
+      <Nochannelselected />
     </Box>
   )
 }
